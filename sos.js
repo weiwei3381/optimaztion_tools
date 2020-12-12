2c1b3046471d8e4bb51b7e6f2c36664151de17cb
wowbat <weiwei3381@live.com>
2020-12-12 18:14:12 +0800

    init: 首次提交

---
+++ sos.js
@@ -0,0 +1,15 @@
+/**
+ * 共生生物搜索算法的javascript实现
+ * @author 小熊
+ * @date 2020-12-12
+ */
+
+class SOS{
+
+  constructor(dimension, ub, lb, fitness_function){
+    this.dimension = dimension
+    this.ub = ub
+    this.lb = lb
+    this.fitness_function = fitness_function
+  }
+}
